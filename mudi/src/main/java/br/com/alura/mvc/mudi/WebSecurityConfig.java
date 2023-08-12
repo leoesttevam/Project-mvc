cdd24b7bf3350ebcf46d0088760be5c3dce18e6b
leoesttevam <leonardoestevam17@gmail.com>
2023-08-12 08:34:50 -0300

    Implementando logout e usando JDBC authentication para salva usuario no banco de dados

--- mudi/src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
@@ -1,43 +1,48 @@
 package br.com.alura.mvc.mudi;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private DataSource dataSource;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-	    http
-	        .authorizeRequests()
-	            .antMatchers("/login").permitAll()
+	    
+		http.authorizeRequests()
 	            .anyRequest().authenticated()
 	            .and()
-	        .formLogin(form -> form
-	            .loginPage("/login")
-	            .permitAll());
+	            .formLogin(form -> form.loginPage("/login")
+	            		.defaultSuccessUrl("/home", true)
+	            .permitAll())
+	            .logout(logout -> logout.logoutUrl("/logout"));
+	            
 	}
-
 	
-	@Bean
 	@Override
-	public UserDetailsService userDetailsService() {
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
-		UserDetails user = User.withDefaultPasswordEncoder()
-						   .username("Leo")
-						   .password("1234")
-						   .roles("ADM")
-						   .build();
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		
-		return new InMemoryUserDetailsManager(user);
-				
+		auth.jdbcAuthentication()
+			.dataSource(dataSource)
+			.passwordEncoder(encoder);
 	}
+	
 }
