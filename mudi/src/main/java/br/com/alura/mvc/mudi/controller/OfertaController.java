82e706fc2e3bb737d9da7c72dcf003baa548403f
leoesttevam <leonardoestevam17@gmail.com>
2023-08-15 20:26:32 -0300

    Usando Vue.js

--- mudi/src/main/java/br/com/alura/mvc/mudi/controller/OfertaController.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/controller/OfertaController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("oferta")
+@RequestMapping("/oferta")
 public class OfertaController {
 
 	@GetMapping
 	public String formularioParaOferta() {
-		return "/oferta/home";
+		return "oferta/home";
 	}
 }
